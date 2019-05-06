972581ecbafecae446cc6f1d3be61f8735d87e27
pkthegod <38172122+pkthegod@users.noreply.github.com>
2019-05-06 20:41:43 -0300

    Commit periódico

---
+++ projeto/turmasprofessor.php
@@ -0,0 +1,116 @@
+<?php require_once("../conexao/conexao.php"); ?>
+
+<?php
+    // teste de segurança
+    session_start();
+    if ( !isset($_SESSION["user_portal"]) ) {
+        header("location:loginprofessor.php");
+    }
+    // fim do teste de seguranca
+
+    // Determinar localidade BR
+    setlocale(LC_ALL, 'pt_BR');
+
+    $professorid = $_SESSION["user_portal"];
+
+    //consulta a tabela turmamodalidades, turmas, dia semana, horários , modalidade.
+
+    $consultatm = "SELECT * FROM turmamodalidades tmd JOIN turmas tm ON tmd.turmaid = tm.turmaID JOIN diasemana ds ON tm.diasemana = ds.diaID JOIN horarios hrs ON tm.horario = hrs.horarioid JOIN modalidades md ON tmd.modalidadeid = md.modalidadeID  WHERE tmd.professorid = {$professorid} ORDER BY ds.diaID ASC ";
+
+
+
+    $resultadotm = mysqli_query($conecta, $consultatm);
+
+  
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Evolução</title>
+        
+        <!-- estilo -->
+        <link href="_css/estilo.css" rel="stylesheet">
+        <link href="_css/produtos.css" rel="stylesheet">
+        <link href="_css/produto_pesquisa.css" rel="stylesheet">
+        
+        
+        
+   
+    </head>
+
+    <body>
+        <?php include_once("_incluir/topoprofessor.php"); ?>
+        
+        <main>
+            
+             <div class="vertical-menu">
+                      <a href="turmasprofessor.php" class="active">Turmas</a>
+                      <a href="chamadaprofessor.php">Chamadas</a>
+                      <a href="#">Calendário</a>
+                      <a href="#">Boletos</a>
+                      
+              </div> 
+            
+            <div> 
+            <?php
+                if(!$resultadotm) {
+                    die("Não há turmas");   
+                }
+                
+                
+            ?>
+                
+                
+                <table border="12">
+                      <tr>
+                        <th>Modalidade</th>
+                        <th>Dia da Semana</th> 
+                        <th>Horário</th>
+                      </tr>
+                         
+                           <?php
+                         while ($dadostm = mysqli_fetch_assoc($resultadotm)){
+                    ?>
+                   
+                      <tr>
+                     
+                        <td>
+                            
+                            
+                          <?php
+                             
+                            echo utf8_encode($dadostm["nomemodalidade"]);       ?></td>
+                            <td><?php
+                                
+                    
+                                echo utf8_encode($dadostm["dia"]);      ?>
+                             
+                             </td>
+                            <td >
+                          
+                          <?php
+                             echo  $dadostm["horarioinicial"] . " " . "às" . " " . $dadostm["horariofinal"];  
+                            
+                             ?>
+                          
+                          
+                             </td>
+                      </tr>
+                      <?php 
+                         }
+                        ?>
+                    </table>
+                      
+            </div>
+            
+        </main>
+
+        <?php include_once("_incluir/rodape.php"); ?>  
+    </body>
+</html>
+
+<?php
+    // Fechar conexao
+    mysqli_close($conecta);
+?>
